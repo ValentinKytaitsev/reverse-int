2677713f75bf42303a5467a260321b1379eefe3b
Valentin Kytaitsev <kytaitsev@gmail.com>
2022-01-26 05:49:04 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 module.exports = function reverse (n) {
+    if (n < 0) {
+        n = n * (-1)
+    }
+    
     let newStr = ""
     let nStr = n.toString()
     let strLength = n.toString().length
     let k = 1
     
-    if (n < 0) {
-        n = n * (-1)
-    }
     if (nStr.endsWith("0")) {
         nStr = nStr.slice(0, -1)
     }
