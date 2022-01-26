93c05e8a642606cee2dc0e22221e4ecc0e7185ac
Valentin Kytaitsev <kytaitsev@gmail.com>
2022-01-26 05:45:44 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 module.exports = function reverse (n) {
-  let newStr = ""
-    const strLength = n.toString().length
-    for (let i=0; i < strLength; i++) {
-        newStr += strLength - i
+    let newStr = ""
+    let nStr = n.toString()
+    let strLength = n.toString().length
+    let k = 1
+    
+    if (n < 0) {
+        n = n * (-1)
     }
-    return Number(newStr)
+    if (nStr.endsWith("0")) {
+        nStr = nStr.slice(0, -1)
+    }
+    for (let i = 0; i < strLength; i++) {
+        newStr += nStr.charAt(strLength - k)
+        k++
+    }
+    return newStr
 }
