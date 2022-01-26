473980ed99b78ea326b46b37335c6396c8bf4dea
Valentin Kytaitsev <kytaitsev@gmail.com>
2022-01-26 02:01:10 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let newStr = ""
+    const strLength = n.toString().length
+    for (let i=0; i < strLength; i++) {
+        newStr += strLength - i
+    }
+    return newStr
 }
